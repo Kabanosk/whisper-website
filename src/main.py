2d9f6c8049586f4e7dfde759f44e8d2092eca4d0
kabanosk <fiolkawojciech@gmail.com>
2022-09-28 00:06:06 +0200

    Add opportunity to make model choice

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import io
+import asyncio
 
-from fastapi import FastAPI, Request, File, UploadFile
+from fastapi import FastAPI, Request, File, Query
 from fastapi.staticfiles import StaticFiles
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -9,8 +10,6 @@ import librosa
 from whisper import whisper
 
 app = FastAPI()
-
-model = whisper.load_model('base')
 
 app.mount('/static', StaticFiles(directory='static'), name='static')
 template = Jinja2Templates(directory='templates')
@@ -25,6 +24,10 @@ def index(request: Request):
 def add_audio(request: Request, file: bytes = File()):
     with open('audio.mp3', 'wb') as f:
         f.write(file)
+    
+    data = request.form()
+    model_type = asyncio.run(data)['model_type']
+    model = whisper.load_model(model_type)
     result = model.transcribe("audio.mp3")
 
     return template.TemplateResponse('index.html',  {"request": request, "text": result['text']})
