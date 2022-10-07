179ac94211575af40dfadbfaffc72e18a3a16812
Wojciech Fiołka <56364007+Kabanosk@users.noreply.github.com>
2022-10-07 13:45:08 +0200

    Update windows_run.bat

--- src/windows_run.bat
+++ src/windows_run.bat
@@ -1,5 +1,5 @@
 @echo off
 
-python3 "-m" "pip" "install" "-r" "..\requirements.txt"
+python "-m" "pip" "install" "-r" "..\requirements.txt"
 git "clone" "git@github.com:openai\whisper.git"
-python3 "run.py"
+python "run.py"
